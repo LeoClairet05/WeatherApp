99817ea94cd24b4a1e28e46bc35e7d8ad44f2ff8
Léo Clairet <leo.clairet@gmail.com>
2021-01-08 11:51:04 +0100

    Search bar in header

--- src/App.js
+++ src/App.js
@@ -7,13 +7,16 @@ function App() {
       <header>
 
         <div className="searchBox">
-          <button className="openBar">
+          {/* <button className="openBar">
             <svg width="27" height="28" viewBox="0 0 27 28" fill="none" xmlns="http://www.w3.org/2000/svg">
               <circle cx="10" cy="10" r="9" stroke="white" stroke-width="2"/>
               <rect x="15" y="17.4142" width="2" height="14" rx="1" transform="rotate(-45 15 17.4142)" fill="white"/>
             </svg>
-          </button>
-          
+          </button> */}
+          <div className="search-box">
+            <input type="text" placeholder="Rechercher une ville"/>
+            <span></span>
+          </div>
         </div>
         
 
